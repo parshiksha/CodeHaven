7ae8b5550546c699ba08a6abfceebcd36f13db7c
Rohit Saini <sidsaini1196@gmail.com>
2020-05-31 08:05:01 +0530

    updated docerfilev2

--- parshiksha/compiler/views.py
+++ parshiksha/compiler/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import parso
+
 
 
 # Create your views here.
 def index(request):
     demoCode = '1+1+1+1+1+1+1+1'
-    module = parso.parse(demoCode, version="3.6")
-    #print(module)
-    expr = module.children[0]
-    print(expr.get_code())
-    f= open("codeGG.py","w+")
-    print('File created')
-    f.write(expr.get_code())
-    f.close() 
-
-
     return HttpResponse("Welcome to Compiler")
 
 
